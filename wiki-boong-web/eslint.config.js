e365f416a3289dac72f93287cefdf183dc1dc366
HongSaek-Ko <bh1152@naver.com>
2025-03-03 23:26:03 +0900

    기존 프로젝트 복사 + 최적화

--- wiki-boong-web/eslint.config.js
+++ wiki-boong-web/eslint.config.js
@@ -1,7 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
+import tailwindcss from 'prettier-plugin-tailwindcss';
 
 export default [
   { ignores: ['dist'] },
@@ -16,18 +20,28 @@ export default [
         sourceType: 'module',
       },
     },
+    settings: { react: { version: '18.3' } },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
+      'tailwindcss': tailwindcss
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'warn',
+      'react/prop-types': 'off',
+      'no-unused-vars': 'warn',
     },
   },
-]
+  eslintConfigPrettier,
+];
