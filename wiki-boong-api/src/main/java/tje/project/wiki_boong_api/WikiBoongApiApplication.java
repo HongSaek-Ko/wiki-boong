6db3d5ea90a8f1a07e4aaba461413d24c52a6933
HongSaek-Ko <bh1152@naver.com>
2025-03-03 21:05:31 +0900

    기존 프로젝트 복사

---
+++ wiki-boong-api/src/main/java/tje/project/wiki_boong_api/WikiBoongApiApplication.java
@@ -0,0 +1,13 @@
+package tje.project.wiki_boong_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiBoongApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiBoongApiApplication.class, args);
+	}
+
+}
---
+++ wiki-boong-api/src/test/java/tje/project/wiki_boong_api/WikiBoongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tje.project.wiki_boong_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiBoongApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
